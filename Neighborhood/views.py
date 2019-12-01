4082545709059baba90e48c8743bb1b43c50c7c3
Dickson Karis <dicksonkariuki4@gmail.com>
2019-12-01 08:54:48 +0300

    Add a view to create new business

--- Neighborhood/views.py
+++ Neighborhood/views.py
@@ -37,5 +37,44 @@ def new_profile(request):
         form = ProfileForm()
     return render(request, 'profile/new_profile.html', {"form": form})
 
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    business = Business.objects.filter(business_owner = current_user)
+    posts = Post.objects.filter(post_owner = current_user)
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile/profile.html',{'profile':prof,'business':business,'posts':posts})
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.business_owner = current_user
+            business.save()
+        return redirect('home')
+    else:
+        form = BusinessForm()
+    return render(request,"business/business_form.html",{"form":form})
+
 
 # Create your views here.
