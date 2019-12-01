f6c616259c81317c7f2156c9509eeedc7d760645
Dickson Karis <dicksonkariuki4@gmail.com>
2019-12-01 08:52:56 +0300

    Add a view to create profile

--- Neighborhood/views.py
+++ Neighborhood/views.py
@@ -22,4 +22,20 @@ def home(request):
 
     return render(request,'hood_app/index.html',{"all_posts":all_posts,"prof_info":profile,"hoodie":hoodie})
 
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/new_profile.html', {"form": form})
+
+
 # Create your views here.
