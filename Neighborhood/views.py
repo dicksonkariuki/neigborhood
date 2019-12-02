c93ce3f9aff79ebb0abcf5de51129f4c9e9606b8
Dickson Karis <dicksonkariuki4@gmail.com>
2019-12-02 14:08:35 +0300

    Add hood contacts

--- Neighborhood/views.py
+++ Neighborhood/views.py
@@ -108,8 +108,7 @@ def search_post(request):
 
 @login_required(login_url='/accounts/login/')
 def contact(request):
-    # contacts = ContactInfo.objects.all()
-    all_contact = ContactInfo.objects.all()
+    contact = ContactInfo.objects.all()
     return render(request,'Neigborhood/contact_info.html',{"contact":contact})
 
 @login_required(login_url='/accounts/login/')
