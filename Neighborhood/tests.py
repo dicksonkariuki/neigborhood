74fced46db968c7e17d3eac2aac272e9d53b2747
Dickson Karis <dicksonkariuki4@gmail.com>
2019-12-02 11:59:22 +0300

    Add test for saving neigborhood

--- Neighborhood/tests.py
+++ Neighborhood/tests.py
@@ -5,7 +5,12 @@ import datetime as dt
 
 class NeighborhoodTestClass(TestCase):
     def setUp(self):
-        self.hood_name=Neighborhood(hood_name="Runda")
+        self.hoods=Neighborhood(hood_name="Runda",hood_location="kitisuru",occupants=21)
     
     def test_instance(self):
-        self.assertTrue(isinstance(self.hood_name,Neighborhood))
+        self.assertTrue(isinstance(self.hoods,Neighborhood))
+
+    def test_save_method(self):
+        self.hoods.save_hood()
+        hoods=Neighborhood.objects.all()
+        self.assertTrue(len(hoods)>0)
