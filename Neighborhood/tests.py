1cb1d6adb3b91dfcca40ee7897bcc7d88b9b67c5
Dickson Karis <dicksonkariuki4@gmail.com>
2019-12-02 12:33:22 +0300

    Add profile test

--- Neighborhood/tests.py
+++ Neighborhood/tests.py
@@ -28,3 +28,21 @@ class NeighborhoodTestClass(TestCase):
         neighborhood.hoods="syokimau"
         self.assertTrue(neighborhood.hood_name,"syokimau")
 
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='password')
+        self.new_profile = Profile(id=1,prof_user=self.new_user,user_location='Turkana')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile(self):
+        self.new_profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
